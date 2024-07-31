a7d3bdebd46aecac835b059c0134a04cd514230c
AnthonyW90 <anthonyt.wallace@icloud.com>
2024-07-31 10:22:16 -0700

    Add gameSuggestion schema

--- server/src/models/schema.ts
+++ server/src/models/schema.ts
@@ -22,3 +22,12 @@ export const userGroups = sqliteTable('user_groups', {
 }, (table) => ({
   pk: primaryKey({ columns: [table.userId, table.groupId] })
 }))
+
+export const gameSuggestions = sqliteTable('game_suggestions', {
+  id: text('id').primaryKey(),
+  gameName: text('game_name').notNull(),
+  suggestedFor: integer('suggested_for', {mode: 'timestamp'}).notNull(),
+  groupId: text('group_id').notNull().references(() => groups.id),
+  userId: text('user_id').notNull().references(() => users.id),
+  createdAt: integer('created_at', { mode: 'timestamp' }).notNull().default(sql`now()`)
+})
